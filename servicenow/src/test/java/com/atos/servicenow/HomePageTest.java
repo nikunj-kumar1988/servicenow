2d86dea4987bdaa87082237de9b06a835670d270
Nikunj Kumar <nikunjkumar1988@gmail.com>
2018-08-19 08:53:13 +0530

    pom.xml updated with testng jars and updated package name in Homepage test

--- servicenow/src/test/java/com/atos/servicenow/HomePageTest.java
+++ servicenow/src/test/java/com/atos/servicenow/HomePageTest.java
@@ -5,11 +5,11 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.crm.qa.base.TestBase;
-import com.crm.qa.pages.ContactsPage;
-import com.crm.qa.pages.HomePage;
-import com.crm.qa.pages.LoginPage;
-import com.crm.qa.util.TestUtil;
+import com.atos.servicenow.base.TestBase;
+import com.atos.servicenow.pages.ContactsPage;
+import com.atos.servicenow.pages.HomePage;
+import com.atos.servicenow.pages.LoginPage;
+import com.atos.servicenow.util.TestUtil;
 
 public class HomePageTest extends TestBase {
 	LoginPage loginPage;
@@ -21,11 +21,11 @@ public class HomePageTest extends TestBase {
 		super();
 	}
 
-	//test cases should be separated -- independent with each other
-	//before each test case -- launch the browser and login
-	//@test -- execute test case
-	//after each test case -- close the browser
-	
+	// test cases should be separated -- independent with each other
+	// before each test case -- launch the browser and login
+	// @test -- execute test case
+	// after each test case -- close the browser
+
 	@BeforeMethod
 	public void setUp() {
 		initialization();
@@ -34,33 +34,28 @@ public class HomePageTest extends TestBase {
 		loginPage = new LoginPage();
 		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
 	}
-	
-	
-	@Test(priority=1)
-	public void verifyHomePageTitleTest(){
+
+	@Test(priority = 1)
+	public void verifyHomePageTitleTest() {
 		String homePageTitle = homePage.verifyHomePageTitle();
-		Assert.assertEquals(homePageTitle, "CRMPRO","Home page title not matched");
+		Assert.assertEquals(homePageTitle, "CRMPRO", "Home page title not matched");
 	}
-	
-	@Test(priority=2)
-	public void verifyUserNameTest(){
+
+	@Test(priority = 2)
+	public void verifyUserNameTest() {
 		testUtil.switchToFrame();
 		Assert.assertTrue(homePage.verifyCorrectUserName());
 	}
-	
-	@Test(priority=3)
-	public void verifyContactsLinkTest(){
+
+	@Test(priority = 3)
+	public void verifyContactsLinkTest() {
 		testUtil.switchToFrame();
 		contactsPage = homePage.clickOnContactsLink();
 	}
-	
-	
-	
+
 	@AfterMethod
-	public void tearDown(){
+	public void tearDown() {
 		driver.quit();
 	}
-	
-	
 
 }
