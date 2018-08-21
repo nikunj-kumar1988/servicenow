2527f76349895755846614ed9eceda7109ee97db
Nikunj Kumar <30670348+nikunj-kumar1988@users.noreply.github.com>
2018-08-21 09:45:54 +0530

    tesnt-ng modified

--- servicenow/src/main/java/com/atos/servicenow/extentreportlistener/ExtentReporterGenerator.java
+++ servicenow/src/main/java/com/atos/servicenow/extentreportlistener/ExtentReporterGenerator.java
@@ -22,7 +22,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
-public class ExtentReporterNG {
+public class ExtentReporterGenerator {
 	    ExtentHtmlReporter htmlReporter;
 	    ExtentReports extent;
 	    ExtentTest test;
