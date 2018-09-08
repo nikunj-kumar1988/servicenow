6f6cd947e469ba0021737616f93867fc8f2c117d
Nikunj Kumar <nikunjkumar1988@gmail.com>
2018-09-08 19:53:21 +0530

    Sysout is added by Nikunj

--- servicenow/src/main/java/com/atos/servicenow/base/TestBase.java
+++ servicenow/src/main/java/com/atos/servicenow/base/TestBase.java
@@ -76,6 +76,7 @@ public class TestBase {
 		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
 
 		driver.get(prop.getProperty("url"));
+		System.out.println("This change is by nikunj");
 	}
 	
 
